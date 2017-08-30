57ae8b1cf0112571a4a7ff70f650af5d807526f4
Brian Lee <bdlee420@yahoo.com>
2017-08-30 13:26:17 -0500

    Added auto select for the date when using displayDate

--- src/app/day-calendar/day-calendar.component.ts
+++ src/app/day-calendar/day-calendar.component.ts
@@ -1,235 +1,238 @@
-import {ECalendarValue} from '../common/types/calendar-value-enum';
-import {SingleCalendarValue} from '../common/types/single-calendar-value';
-import {ECalendarMode} from '../common/types/calendar-mode-enum';
+﻿import { ECalendarValue } from '../common/types/calendar-value-enum';
+import { SingleCalendarValue } from '../common/types/single-calendar-value';
+import { ECalendarMode } from '../common/types/calendar-mode-enum';
 import {
-  Component,
-  EventEmitter,
-  forwardRef,
-  HostBinding,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges
+	Component,
+	EventEmitter,
+	forwardRef,
+	HostBinding,
+	Input,
+	OnChanges,
+	OnInit,
+	Output,
+	SimpleChanges
 } from '@angular/core';
-import {DayCalendarService} from './day-calendar.service';
+import { DayCalendarService } from './day-calendar.service';
 import * as moment from 'moment';
-import {Moment} from 'moment';
-import {IDayCalendarConfig} from './day-calendar-config.model';
-import {IDay} from './day.model';
+import { Moment } from 'moment';
+import { IDayCalendarConfig } from './day-calendar-config.model';
+import { IDay } from './day.model';
 import {
-  ControlValueAccessor,
-  FormControl,
-  NG_VALIDATORS,
-  NG_VALUE_ACCESSOR,
-  ValidationErrors,
-  Validator
+	ControlValueAccessor,
+	FormControl,
+	NG_VALIDATORS,
+	NG_VALUE_ACCESSOR,
+	ValidationErrors,
+	Validator
 } from '@angular/forms';
-import {CalendarValue} from '../common/types/calendar-value';
-import {UtilsService} from '../common/services/utils/utils.service';
-import {IMonthCalendarConfig} from '../month-calendar/month-calendar-config';
-import {IMonth} from '../month-calendar/month.model';
+import { CalendarValue } from '../common/types/calendar-value';
+import { UtilsService } from '../common/services/utils/utils.service';
+import { IMonthCalendarConfig } from '../month-calendar/month-calendar-config';
+import { IMonth } from '../month-calendar/month.model';
 
 @Component({
-  selector: 'dp-day-calendar',
-  templateUrl: 'day-calendar.component.html',
-  styleUrls: ['day-calendar.component.less'],
-  providers: [
-    DayCalendarService,
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => DayCalendarComponent),
-      multi: true
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: forwardRef(() => DayCalendarComponent),
-      multi: true
-    }
-  ]
+	selector: 'dp-day-calendar',
+	templateUrl: 'day-calendar.component.html',
+	styleUrls: ['day-calendar.component.less'],
+	providers: [
+		DayCalendarService,
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => DayCalendarComponent),
+			multi: true
+		},
+		{
+			provide: NG_VALIDATORS,
+			useExisting: forwardRef(() => DayCalendarComponent),
+			multi: true
+		}
+	]
 })
 export class DayCalendarComponent implements OnInit, OnChanges, ControlValueAccessor, Validator {
 
-  @Input() config: IDayCalendarConfig;
-  @Input() displayDate: SingleCalendarValue;
-  @Input() minDate: Moment;
-  @Input() maxDate: Moment;
-  @HostBinding('class') @Input() theme: string;
-  @Output() onSelect: EventEmitter<IDay> = new EventEmitter();
-  @Output() onMonthSelect: EventEmitter<IMonth> = new EventEmitter();
-  @Output() onNavHeaderBtnClick: EventEmitter<ECalendarMode> = new EventEmitter();
+	@Input() config: IDayCalendarConfig;
+	@Input() displayDate: SingleCalendarValue;
+	@Input() minDate: Moment;
+	@Input() maxDate: Moment;
+	@HostBinding('class') @Input() theme: string;
+	@Output() onSelect: EventEmitter<IDay> = new EventEmitter();
+	@Output() onMonthSelect: EventEmitter<IMonth> = new EventEmitter();
+	@Output() onNavHeaderBtnClick: EventEmitter<ECalendarMode> = new EventEmitter();
 
-  CalendarMode = ECalendarMode;
-  isInited: boolean = false;
-  componentConfig: IDayCalendarConfig;
-  _selected: Moment[];
-  weeks: IDay[][];
-  weekdays: Moment[];
-  currentDateView: Moment;
-  inputValue: CalendarValue;
-  inputValueType: ECalendarValue;
-  validateFn: (inputVal: CalendarValue) => { [key: string]: any };
-  currentCalendarMode: ECalendarMode = ECalendarMode.Day;
-  monthCalendarConfig: IMonthCalendarConfig;
+	CalendarMode = ECalendarMode;
+	isInited: boolean = false;
+	componentConfig: IDayCalendarConfig;
+	_selected: Moment[];
+	weeks: IDay[][];
+	weekdays: Moment[];
+	currentDateView: Moment;
+	inputValue: CalendarValue;
+	inputValueType: ECalendarValue;
+	validateFn: (inputVal: CalendarValue) => { [key: string]: any };
+	currentCalendarMode: ECalendarMode = ECalendarMode.Day;
+	monthCalendarConfig: IMonthCalendarConfig;
 
-  api = {
-    moveCalendarsBy: this.moveCalendarsBy.bind(this),
-    toggleCalendar: this.toggleCalendar.bind(this)
-  };
+	api = {
+		moveCalendarsBy: this.moveCalendarsBy.bind(this),
+		toggleCalendar: this.toggleCalendar.bind(this)
+	};
 
-  set selected(selected: Moment[]) {
-    this._selected = selected;
-    this.onChangeCallback(this.processOnChangeCallback(selected));
-  }
+	set selected(selected: Moment[]) {
+		this._selected = selected;
+		this.onChangeCallback(this.processOnChangeCallback(selected));
+	}
 
-  get selected(): Moment[] {
-    return this._selected;
-  }
+	get selected(): Moment[] {
+		return this._selected;
+	}
 
-  constructor(public dayCalendarService: DayCalendarService,
-              public utilsService: UtilsService) {
-  }
+	constructor(public dayCalendarService: DayCalendarService,
+		public utilsService: UtilsService) {
+	}
 
-  ngOnInit() {
-    this.isInited = true;
-    this.init();
-    this.initValidators();
-  }
+	ngOnInit() {
+		this.isInited = true;
+		this.init();
+		this.initValidators();
+	}
 
-  init() {
-    this.componentConfig = this.dayCalendarService.getConfig(this.config);
-    this.selected = this.selected || [];
-    this.currentDateView = this.displayDate
-      ? this.utilsService.convertToMoment(this.displayDate, this.componentConfig.format).clone()
-      : this.utilsService
-        .getDefaultDisplayDate(this.currentDateView, this.selected, this.componentConfig.allowMultiSelect);
-    this.weeks = this.dayCalendarService
-      .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-    this.weekdays = this.dayCalendarService
-      .generateWeekdays(this.componentConfig.firstDayOfWeek);
-    this.inputValueType = this.utilsService.getInputType(this.inputValue, this.componentConfig.allowMultiSelect);
-    this.monthCalendarConfig = this.dayCalendarService.getMonthCalendarConfig(this.componentConfig);
-  }
+	init() {
+		this.componentConfig = this.dayCalendarService.getConfig(this.config);
+		this.selected = this.selected || [];
+		if (this.displayDate)
+			this.selected.push(this.displayDate);
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (this.isInited) {
-      const {minDate, maxDate} = changes;
-      this.init();
+		this.currentDateView = this.displayDate
+			? this.utilsService.convertToMoment(this.displayDate, this.componentConfig.format).clone()
+			: this.utilsService
+				.getDefaultDisplayDate(this.currentDateView, this.selected, this.componentConfig.allowMultiSelect);
+		this.weeks = this.dayCalendarService
+			.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+		this.weekdays = this.dayCalendarService
+			.generateWeekdays(this.componentConfig.firstDayOfWeek);
+		this.inputValueType = this.utilsService.getInputType(this.inputValue, this.componentConfig.allowMultiSelect);
+		this.monthCalendarConfig = this.dayCalendarService.getMonthCalendarConfig(this.componentConfig);
+	}
 
-      if (minDate || maxDate) {
-        this.initValidators();
-      }
-    }
-  }
+	ngOnChanges(changes: SimpleChanges) {
+		if (this.isInited) {
+			const { minDate, maxDate } = changes;
+			this.init();
 
-  writeValue(value: CalendarValue): void {
-    this.inputValue = value;
+			if (minDate || maxDate) {
+				this.initValidators();
+			}
+		}
+	}
 
-    if (value) {
-      this.selected = this.utilsService
-        .convertToMomentArray(value, this.componentConfig.format, this.componentConfig.allowMultiSelect);
-      this.weeks = this.dayCalendarService
-        .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-      this.inputValueType = this.utilsService
-        .getInputType(this.inputValue, this.componentConfig.allowMultiSelect);
-    } else {
-      this.selected = [];
-      this.weeks = this.dayCalendarService
-        .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-    }
-  }
+	writeValue(value: CalendarValue): void {
+		this.inputValue = value;
 
-  registerOnChange(fn: any): void {
-    this.onChangeCallback = fn;
-  }
+		if (value) {
+			this.selected = this.utilsService
+				.convertToMomentArray(value, this.componentConfig.format, this.componentConfig.allowMultiSelect);
+			this.weeks = this.dayCalendarService
+				.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+			this.inputValueType = this.utilsService
+				.getInputType(this.inputValue, this.componentConfig.allowMultiSelect);
+		} else {
+			this.selected = [];
+			this.weeks = this.dayCalendarService
+				.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+		}
+	}
 
-  onChangeCallback(_: any) {
-  };
+	registerOnChange(fn: any): void {
+		this.onChangeCallback = fn;
+	}
 
-  registerOnTouched(fn: any): void {
-  }
+	onChangeCallback(_: any) {
+	};
 
-  validate(formControl: FormControl): ValidationErrors | any {
-    if (this.minDate || this.maxDate) {
-      return this.validateFn(formControl.value);
-    } else {
-      return () => null;
-    }
-  }
+	registerOnTouched(fn: any): void {
+	}
 
-  processOnChangeCallback(value: Moment[]): CalendarValue {
-    return this.utilsService.convertFromMomentArray(this.componentConfig.format, value, this.inputValueType);
-  }
+	validate(formControl: FormControl): ValidationErrors | any {
+		if (this.minDate || this.maxDate) {
+			return this.validateFn(formControl.value);
+		} else {
+			return () => null;
+		}
+	}
 
-  initValidators() {
-    this.validateFn = this.utilsService.createValidator(
-      {minDate: this.minDate, maxDate: this.maxDate}, this.componentConfig.format, 'day');
+	processOnChangeCallback(value: Moment[]): CalendarValue {
+		return this.utilsService.convertFromMomentArray(this.componentConfig.format, value, this.inputValueType);
+	}
 
-    this.onChangeCallback(this.processOnChangeCallback(this.selected));
-  }
+	initValidators() {
+		this.validateFn = this.utilsService.createValidator(
+			{ minDate: this.minDate, maxDate: this.maxDate }, this.componentConfig.format, 'day');
 
-  isDisabledDay(day: IDay) {
-    return this.dayCalendarService.isDateDisabled(day, this.componentConfig);
-  }
+		this.onChangeCallback(this.processOnChangeCallback(this.selected));
+	}
 
-  dayClicked(day: IDay) {
-    this.selected = this.utilsService
-      .updateSelected(this.componentConfig.allowMultiSelect, this.selected, day);
-    this.weeks = this.dayCalendarService
-      .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-    this.onSelect.emit(day);
-  }
+	isDisabledDay(day: IDay) {
+		return this.dayCalendarService.isDateDisabled(day, this.componentConfig);
+	}
 
-  getNavLabel(): string {
-    return this.dayCalendarService.getHeaderLabel(this.componentConfig, this.currentDateView);
-  }
+	dayClicked(day: IDay) {
+		this.selected = this.utilsService
+			.updateSelected(this.componentConfig.allowMultiSelect, this.selected, day);
+		this.weeks = this.dayCalendarService
+			.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+		this.onSelect.emit(day);
+	}
 
-  getDayBtnText(day: IDay): string {
-    return this.dayCalendarService.getDayBtnText(this.componentConfig, day.date);
-  }
+	getNavLabel(): string {
+		return this.dayCalendarService.getHeaderLabel(this.componentConfig, this.currentDateView);
+	}
 
-  onLeftNav() {
-    this.currentDateView.subtract(1, 'month');
-    this.weeks = this.dayCalendarService
-      .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-  }
+	getDayBtnText(day: IDay): string {
+		return this.dayCalendarService.getDayBtnText(this.componentConfig, day.date);
+	}
 
-  onRightNav() {
-    this.currentDateView.add(1, 'month');
-    this.weeks = this.dayCalendarService
-      .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-  }
+	onLeftNav() {
+		this.currentDateView.subtract(1, 'month');
+		this.weeks = this.dayCalendarService
+			.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+	}
 
-  shouldShowLeftNav(): boolean {
-    return this.dayCalendarService.shouldShowLeft(this.componentConfig.min, this.currentDateView);
-  }
+	onRightNav() {
+		this.currentDateView.add(1, 'month');
+		this.weeks = this.dayCalendarService
+			.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+	}
 
-  shouldShowRightNav(): boolean {
-    return this.dayCalendarService.shouldShowRight(this.componentConfig.max, this.currentDateView);
-  }
+	shouldShowLeftNav(): boolean {
+		return this.dayCalendarService.shouldShowLeft(this.componentConfig.min, this.currentDateView);
+	}
 
-  isNavHeaderBtnClickable(): boolean {
-    return this.componentConfig.enableMonthSelector;
-  }
+	shouldShowRightNav(): boolean {
+		return this.dayCalendarService.shouldShowRight(this.componentConfig.max, this.currentDateView);
+	}
 
-  toggleCalendar(mode: ECalendarMode) {
-    if (this.currentCalendarMode !== mode) {
-      this.currentCalendarMode = mode;
-      this.onNavHeaderBtnClick.emit(mode);
-    }
-  }
+	isNavHeaderBtnClickable(): boolean {
+		return this.componentConfig.enableMonthSelector;
+	}
 
-  monthSelected(month: IMonth) {
-    this.currentDateView = month.date.clone();
-    this.currentCalendarMode = ECalendarMode.Day;
-    this.weeks = this.dayCalendarService
-      .generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
-    this.onMonthSelect.emit(month);
-  }
+	toggleCalendar(mode: ECalendarMode) {
+		if (this.currentCalendarMode !== mode) {
+			this.currentCalendarMode = mode;
+			this.onNavHeaderBtnClick.emit(mode);
+		}
+	}
 
-  moveCalendarsBy(current: Moment, amount: number, granularity: moment.unitOfTime.Base = 'month') {
-    const to = current.add(amount, granularity);
-    this.currentDateView = to;
-    this.weeks = this.dayCalendarService.generateMonthArray(this.componentConfig, to, this.selected);
-  }
+	monthSelected(month: IMonth) {
+		this.currentDateView = month.date.clone();
+		this.currentCalendarMode = ECalendarMode.Day;
+		this.weeks = this.dayCalendarService
+			.generateMonthArray(this.componentConfig, this.currentDateView, this.selected);
+		this.onMonthSelect.emit(month);
+	}
+
+	moveCalendarsBy(current: Moment, amount: number, granularity: moment.unitOfTime.Base = 'month') {
+		const to = current.add(amount, granularity);
+		this.currentDateView = to;
+		this.weeks = this.dayCalendarService.generateMonthArray(this.componentConfig, to, this.selected);
+	}
 }
